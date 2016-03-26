fd09de0390a84511252a9a2c4853faeeb307431e
Wim <wim@sitebase.be>
2016-03-26 21:27:07 +0100

    modules based system working

--- src/demo.js
+++ src/demo.js
@@ -8,68 +8,68 @@ export default class Demo extends React.Component {
     render() {
         return (
             <div>
-                <section>
-                    <h2>Gravatar</h2>
-                    <Avatar className="myCustomClass" email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={40}></Avatar>
-                    <Avatar email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={100} round={true}></Avatar>
-                    <Avatar email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={150}></Avatar>
-                    <Avatar email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={200}></Avatar>
-                </section>
+      <section>
+        <h2>Gravatar</h2>
+        <Avatar className="myCustomClass" md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={40}></Avatar>
+        <Avatar md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={100} round={true}></Avatar>
+        <Avatar md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={150}></Avatar>
+        <Avatar md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={200}></Avatar>
+      </section>
 
-                <section>
-                    <h2>Invalid gravatar</h2>
-                    <Avatar email="bla" name="Jim Jones" size={80}></Avatar>
-                </section>
+      <section>
+        <h2>Invalid gravatar</h2>
+        <Avatar email="bla" name="Jim Jones" size={80}></Avatar>
+      </section>
 
-                <section>
-                    <h2>Google+</h2>
-                    <Avatar googleId="116933859726289749306" size={40}></Avatar>
-                    <Avatar googleId="116933859726289749306" size={100} round={true}></Avatar>
-                    <Avatar googleId="116933859726289749306" size={150}></Avatar>
-                    <Avatar googleId="116933859726289749306" size={200}></Avatar>
-                </section>
+      <section>
+        <h2>Google+</h2>
+        <Avatar googleId="116933859726289749306" size={40}></Avatar>
+        <Avatar googleId="116933859726289749306" size={100} round={true}></Avatar>
+        <Avatar googleId="116933859726289749306" size={150}></Avatar>
+        <Avatar googleId="116933859726289749306" size={200}></Avatar>
+      </section>
 
-                <section>
-                    <h2>Facebook</h2>
-                    <Avatar facebookId="100008343750912" size={40}></Avatar>
-                    <Avatar facebookId="100008343750912" size={100} round={true}></Avatar>
-                    <Avatar facebookId="100008343750912" size={150}></Avatar>
-                    <Avatar facebookId="100008343750912" size={200}></Avatar>
-                </section>
+      <section>
+        <h2>Facebook</h2>
+        <Avatar facebookId="100008343750912" size={40}></Avatar>
+        <Avatar facebookId="100008343750912" size={100} round={true}></Avatar>
+        <Avatar facebookId="100008343750912" size={150}></Avatar>
+        <Avatar facebookId="100008343750912" size={200}></Avatar>
+      </section>
 
-                <section>
-                    <h2>Skype</h2>
-                    <Avatar skypeId="sitebase" size={40}></Avatar>
-                    <Avatar skypeId="sitebase" size={100} round={true}></Avatar>
-                    <Avatar skypeId="sitebase" size={150}></Avatar>
-                    <Avatar skypeId="sitebase" size={200}></Avatar>
-                </section>
+      <section>
+        <h2>Skype</h2>
+        <Avatar skypeId="sitebase" size={40}></Avatar>
+        <Avatar skypeId="sitebase" size={100} round={true}></Avatar>
+        <Avatar skypeId="sitebase" size={150}></Avatar>
+        <Avatar skypeId="sitebase" size={200}></Avatar>
+      </section>
 
-                <section>
-                    <h2>Initials</h2>
-                    <Avatar color="#00FF00" name="Wim Mostmans" size={40}></Avatar>
-                    <Avatar name="Wim Mostmans" size={100} round={true}></Avatar>
-                    <Avatar name="Wim Mostmans" size={150}></Avatar>
-                    <Avatar name="Wim Mostmans" size={200}></Avatar>
-                </section>
+      <section>
+        <h2>Initials</h2>
+        <Avatar color="#00FF00" name="Wim Mostmans" size={40}></Avatar>
+        <Avatar name="Wim Mostmans" size={100} round={true}></Avatar>
+        <Avatar name="Wim Mostmans" size={150}></Avatar>
+        <Avatar name="Wim Mostmans" size={200}></Avatar>
+      </section>
 
-                <section>
-                    <h2>Value</h2>
-                    <Avatar value="86%" size={40}></Avatar>
-                    <Avatar value="86%" size={100} round={true}></Avatar>
-                    <Avatar value="86%" size={150}></Avatar>
-                    <Avatar value="86%" size={200}></Avatar>
-                </section>
+      <section>
+        <h2>Value</h2>
+        <Avatar value="86%" size={40}></Avatar>
+        <Avatar value="86%" size={100} round={true}></Avatar>
+        <Avatar value="86%" size={150}></Avatar>
+        <Avatar value="86%" size={200}></Avatar>
+      </section>
 
-                <section>
-                    <h2>Fallback to static src</h2>
-                    <Avatar size={100} facebookId="invalidfacebookusername" src="http://www.gravatar.com/avatar/a16a38cdfe8b2cbd38e8a56ab93238d3?s=<%=size%>"></Avatar>
-                </section>
+      <section>
+        <h2>Fallback to static src</h2>
+        <Avatar size={100} facebookId="invalidfacebookusername" src="http://superiorplatform.com/pictures-of/faces/angry/04a-angry_baby.jpg"></Avatar>
+      </section>
 
-                <section>
-                    <h2>Double fallback: Facebook to Google to initials</h2>
-                    <Avatar facebookId="invalidfacebookusername" googleId="invalidgoogleid" name="Sitebase" size={200} round={true}></Avatar>
-                </section>
+      <section>
+        <h2>Double fallback: Facebook to Google to initials</h2>
+        <Avatar facebookId="invalidfacebookusername" googleId="invalidgoogleid" name="Sitebase" size={200} round={true}></Avatar>
+      </section>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 'use strict';
 
 import React from 'react';
-import md5 from 'md5';
-import retina from 'is-retina';
+import gravatarSource from './sources/Gravatar';
+import facebookSource from './sources/Facebook';
+import googleSource from './sources/Google';
+import skypeSource from './sources/Skype';
+import valueSource from './sources/Value';
+import srcSource from './sources/Src';
+import iconSource from './sources/Icon';
 
-const IS_RETINA = retina();
 const PROTOCOL = typeof window === 'undefined' ?
                     'https:' : window.location.protocol;
-const IS_HTTPS = PROTOCOL === 'https:';
+const SOURCES = [
+    gravatarSource,
+    facebookSource,
+    googleSource,
+    skypeSource,
+    valueSource,
+    srcSource,
+    iconSource
+];
 
 export default class Avatar extends React.Component {
     static displayName = 'Avatar'
@@ -15,17 +27,17 @@ export default class Avatar extends React.Component {
         className: React.PropTypes.string,
         fgColor: React.PropTypes.string,
         color: React.PropTypes.string,
+        colors: React.PropTypes.array,
         name: React.PropTypes.string,
         value: React.PropTypes.string,
         email: React.PropTypes.string,
+        md5Email: React.PropTypes.string,
         src: React.PropTypes.string,
         facebookId: React.PropTypes.string,
         googleId: React.PropTypes.string,
         skypeId: React.PropTypes.string,
         round: React.PropTypes.bool,
-        size: React.PropTypes.number,
-
-        channelPriority: React.PropTypes.array
+        size: React.PropTypes.number
     }
 
     static defaultProps = {
@@ -34,24 +46,21 @@ export default class Avatar extends React.Component {
         name: null,
         value: null,
         email: null,
+        md5Email: null,
         facebookId: null,
         skypeId: null,
         googleId: null,
         round: false,
-        size: 100,
-
-        channelPriority: Avatar.defaultPriority
+        size: 100
     }
 
     constructor(props) {
         super(props);
         this.state = {
+            sourcePointer: 0,
             src: null,
             value: null,
-            triedFacebook: false,
-            triedGoogle: false,
-            triedSkype: false,
-            triedGravatar: false
+            color: props.color
         };
     }
 
@@ -74,63 +83,21 @@ export default class Avatar extends React.Component {
         }
     }
 
-    static colors = [
-        '#d73d32',
-        '#7e3794',
-        '#4285f4',
-        '#67ae3f',
-        '#d61a7f',
-        '#ff4080'
-    ]
     static gravatarURLTemplate = 'gravatar.com/avatar/<%=id%>?s=<%=size%>&d=404'
     static facebookURLTemplate = 'graph.facebook.com/<%=id%>/picture?width=<%=size%>'
     static googleURLTemplate = 'picasaweb.google.com/data/entry/api/user/<%=id%>?alt=json'
     static skypeURLTemplate = 'api.skype.com/users/<%=id%>/profile/avatar'
 
-    static sourceSrc = 'sourceSrc'
-    static sourceGravatar = 'sourceGravatar'
-    static sourceFacebook = 'sourceFacebook'
-    static sourceGoogle = 'sourceGoogle'
-    static sourceSkype = 'sourceSkype'
-    static sourceName = 'sourceName'
-    static sourceValue = 'sourceValue'
-    static defaultPriority = [
-        Avatar.sourceSrc,
-        Avatar.sourceGravatar,
-        Avatar.sourceFacebook,
-        Avatar.sourceGoogle,
-        Avatar.sourceSkype,
-        Avatar.sourceName,
-        Avatar.sourceValue
-    ]
-
-    getGravatarURL(email, size, cb )
-    {
-
-        // if email does not contain @ it's already an MD5 hash
-        if( email.indexOf('@') > -1 )
-            email = md5(email);
-
-        const prefixURL = IS_HTTPS ? 'https://secure.' : 'http://';
-        const parsedURL = this.parse(Avatar.gravatarURLTemplate, {
-            id: email,
-            size: IS_RETINA ? size * 2 : size
-        });
-
-        cb(prefixURL + parsedURL);
-    }
-
-    getFacebookURL( id, size, cb )
+    getFacebookURL = (callback) =>
     {
         const parsedURL = this.parse(Avatar.facebookURLTemplate, {
-            id: id,
-            size: size
+            id: this.props.facebookId,
+            size: this.props.size
         });
-
-        cb(`${PROTOCOL}//${parsedURL}`);
+        callback(`${PROTOCOL}//${parsedURL}`);
     }
 
-    getGoogleURL( id, size, cb, tryNext )
+    getGoogleURL = ( id, size, cb, tryNext ) =>
     {
         const parsedURL = this.parse(Avatar.googleURLTemplate, {id: id});
         const url = `${PROTOCOL}//${parsedURL}`;
@@ -142,7 +109,7 @@ export default class Avatar extends React.Component {
         }, tryNext);
     }
 
-    getSkypeURL( id, size, cb )
+    getSkypeURL = ( id, size, cb ) =>
     {
         const parsedURL = this.parse(Avatar.skypeURLTemplate, {id: id});
         cb(`${PROTOCOL}//${parsedURL}`);
@@ -157,96 +124,87 @@ export default class Avatar extends React.Component {
         return value;
     }
 
-    rndColor()
-    {
-        const index = Math.floor(Math.random() * Avatar.colors.length);
-        return Avatar.colors[ index ];
-    }
-
-    getInitials( name )
-    {
-        const parts = name.split(' ');
-        let initials = '';
-        for(let i = 0 ; i < parts.length ; i++)
-        {
-            initials += parts[i].substr(0, 1).toUpperCase();
-        }
-        return initials;
-    }
-
-    get(url, successCb, errorCb) {
-        const request = new XMLHttpRequest();
-        request.onreadystatechange = function() {
-            if (request.readyState === 4) {
-                if (request.status === 200) {
-                    const data = JSON.parse(request.responseText);
-                    successCb(data);
-                } else {
-                    errorCb(request.status);
-                }
-            }
-        };
-        request.open('GET', url, true);
-        request.send();
-    }
-
     setSrc = ( src ) => {
         if(src)
             this.setState({src: src});
     }
 
-    fetch = ( e ) => {
-        const url = null;
-        const self = this;
-        const tryNext = function() {
-            self.fetch();
-        };
+    tryNextsource = (Source) => {
 
+        const instance = new Source(this.props);
+
+        if(!instance.isCompatible(this.props)) {
+            console.error('Source function "%s" not found', Source.name);
+            this.fetch();
+            return;
+        }
+
+        instance.get((state) => {
+            if(state) {
+                this.setState(state);
+                return;
+            } else {
+                this.fetch();
+            }
+        });
+    }
+
+    fetch = (event) => {
         // If fetch was triggered by img onError
         // then set state src back to null so getVisual will
         // automatically switch to drawn avatar if there is no other social ID available to try
-        if( e && e.type === 'error' )
-            this.state.src = null;
+        if( event && event.type === 'error' )
+            this.setState({src: null});
 
-        if( this.state.triedFacebook === false && ! this.state.url && this.props.facebookId) {
-            this.state.triedFacebook = true;
-            this.getFacebookURL( this.props.facebookId , this.props.size, this.setSrc, tryNext );
-            return;
-        }
-
-        if( this.state.triedGoogle === false && ! this.state.url && this.props.googleId) {
-            this.state.triedGoogle = true;
-            this.getGoogleURL( this.props.googleId , this.props.size, this.setSrc, tryNext );
-            return;
-        }
-
-        if( this.state.triedSkype === false && ! this.state.url && this.props.skypeId) {
-            this.state.triedSkype = true;
-            this.getSkypeURL( this.props.skypeId , this.props.size, this.setSrc, tryNext );
-            return;
-        }
-
-        if( this.state.triedGravatar === false && ! this.state.url && this.props.email) {
-            this.state.triedGravatar = true;
-            this.getGravatarURL( this.props.email, this.props.size, this.setSrc, tryNext );
-            return;
-        }
-
-        if( this.state.src )
+        if(SOURCES.length === this.state.sourcePointer)
             return;
 
-        if( this.props.name )
-            this.setState({ value: this.getInitials( this.props.name ) });
+        const source = SOURCES[this.state.sourcePointer];
+        this.setState({
+            sourcePointer: (this.state.sourcePointer + 1)
+        }, () => {
+            this.tryNextsource(source);
+        });
 
-        if( !this.props.name && this.props.value )
-            this.setState({ value: this.props.value });
+        // if( this.state.triedFacebook === false && ! this.state.url && this.props.facebookId) {
+            // this.state.triedFacebook = true;
+            // this.getFacebookURL( this.props.facebookId , this.props.size, this.setSrc, tryNext );
+            // return;
+        // }
 
-        if( url === null && this.props.src) {
-            const parsedURL = this.parse(this.props.src, {
-                size: this.props.size
-            });
-            this.setSrc(parsedURL);
-        }
+        // if( this.state.triedGoogle === false && ! this.state.url && this.props.googleId) {
+            // this.state.triedGoogle = true;
+            // this.getGoogleURL( this.props.googleId , this.props.size, this.setSrc, tryNext );
+            // return;
+        // }
+
+        // if( this.state.triedSkype === false && ! this.state.url && this.props.skypeId) {
+            // this.state.triedSkype = true;
+            // this.getSkypeURL( this.props.skypeId , this.props.size, this.setSrc, tryNext );
+            // return;
+        // }
+
+        // if( this.state.triedGravatar === false && ! this.state.url && this.props.email) {
+            // this.state.triedGravatar = true;
+            // this.getGravatarURL( this.props.email, this.props.size, this.setSrc, tryNext );
+            // return;
+        // }
+
+        // if( this.state.src )
+            // return;
+
+        // if( this.props.name )
+            // this.setState({ value: this.getInitials( this.props.name ) });
+
+        // if( !this.props.name && this.props.value )
+            // this.setState({ value: this.props.value });
+
+        // if( url === null && this.props.src) {
+            // const parsedURL = this.parse(this.props.src, {
+                // size: this.props.size
+            // });
+            // this.setSrc(parsedURL);
+        // }
     }
 
     getVisual() {
@@ -260,7 +218,7 @@ export default class Avatar extends React.Component {
         };
 
         const initialsStyle = {
-            background: this.props.color || this.rndColor(),
+            background: this.state.color,
             width: size,
             height: size,
             font: Math.floor(size / 3) + 'px/100px Helvetica, Arial, sans-serif',
