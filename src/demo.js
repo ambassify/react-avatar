cc3ddb04c6682b78fce8d8e4e43bbe7612087180
Wim <wim@sitebase.be>
2016-05-14 17:13:52 +0200

    use self closing components

--- src/demo.js
+++ src/demo.js
@@ -10,60 +10,60 @@ export default class Demo extends React.Component {
             <div>
       <section>
         <h2>Gravatar</h2>
-        <Avatar className="myCustomClass" md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={40}></Avatar>
-        <Avatar md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={100} round={true}></Avatar>
-        <Avatar md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={150}></Avatar>
-        <Avatar md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={200}></Avatar>
+        <Avatar className="myCustomClass" md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={40} />
+        <Avatar md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={100} round={true} />
+        <Avatar md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={150} />
+        <Avatar md5Email="8c5d4c4b9ef6c68c4ff91c319d4c56be" size={200} />
       </section>
 
       <section>
         <h2>Invalid gravatar</h2>
-        <Avatar email="bla" name="Jim Jones" size={80}></Avatar>
+        <Avatar email="bla" name="Jim Jones" size={80} />
       </section>
 
       <section>
         <h2>Google+</h2>
-        <Avatar googleId="116933859726289749306" size={40}></Avatar>
-        <Avatar googleId="116933859726289749306" size={100} round={true}></Avatar>
-        <Avatar googleId="116933859726289749306" size={150}></Avatar>
-        <Avatar googleId="116933859726289749306" size={200}></Avatar>
+        <Avatar googleId="116933859726289749306" size={40} />
+        <Avatar googleId="116933859726289749306" size={100} round={true} />
+        <Avatar googleId="116933859726289749306" size={150} />
+        <Avatar googleId="116933859726289749306" size={200} />
       </section>
 
       <section>
         <h2>Facebook</h2>
-        <Avatar facebookId="100008343750912" size={40}></Avatar>
-        <Avatar facebookId="100008343750912" size={100} round={true}></Avatar>
-        <Avatar facebookId="100008343750912" size={150}></Avatar>
-        <Avatar facebookId="100008343750912" size={200}></Avatar>
+        <Avatar facebookId="100008343750912" size={40} />
+        <Avatar facebookId="100008343750912" size={100} round={true} />
+        <Avatar facebookId="100008343750912" size={150} />
+        <Avatar facebookId="100008343750912" size={200} />
       </section>
 
       <section>
         <h2>Skype</h2>
-        <Avatar skypeId="sitebase" size={40}></Avatar>
-        <Avatar skypeId="sitebase" size={100} round={true}></Avatar>
-        <Avatar skypeId="sitebase" size={150}></Avatar>
-        <Avatar skypeId="sitebase" size={200}></Avatar>
+        <Avatar skypeId="sitebase" size={40} />
+        <Avatar skypeId="sitebase" size={100} round={true} />
+        <Avatar skypeId="sitebase" size={150} />
+        <Avatar skypeId="sitebase" size={200} />
       </section>
 
       <section>
         <h2>Initials</h2>
-        <Avatar color="#00FF00" name="Wim Mostmans" size={40}></Avatar>
-        <Avatar name="Wim Mostmans" size={100} round={true}></Avatar>
-        <Avatar name="Wim Mostmans" size={150}></Avatar>
-        <Avatar name="Wim Mostmans" size={200}></Avatar>
+        <Avatar color="#00FF00" name="Wim Mostmans" size={40} />
+        <Avatar name="Wim Mostmans" size={100} round={true}/>
+        <Avatar name="Wim Mostmans" size={150} />
+        <Avatar name="Wim Mostmans" size={200} />
       </section>
 
-      <section>
+    <section>
         <h2>Value</h2>
-        <Avatar value="86%" size={40}></Avatar>
-        <Avatar value="86%" size={100} round={true}></Avatar>
-        <Avatar value="86%" size={150}></Avatar>
-        <Avatar value="86%" size={200}></Avatar>
+        <Avatar value="86%" size={40} />
+        <Avatar value="86%" size={100} round={true} />
+        <Avatar value="86%" size={150} />
+        <Avatar value="86%" size={200} />
       </section>
 
       <section>
         <h2>Fallback to static src</h2>
-        <Avatar size={100} facebookId="invalidfacebookusername" src="http://superiorplatform.com/pictures-of/faces/angry/04a-angry_baby.jpg"></Avatar>
+        <Avatar size={100} facebookId="invalidfacebookusername" src="http://superiorplatform.com/pictures-of/faces/angry/04a-angry_baby.jpg" />
       </section>
 
       <section>
