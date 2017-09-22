bf5e3c162dbd179e1abdfd65e123f6fb0fe2e3af
Vladimir Zaburdaev <v.zaburdaev@gmail.com>
2017-09-22 16:25:46 +0300

    Change tabs to spaces

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ export default class Avatar extends React.Component {
         size: PropTypes.number,
         textSizeRatio: PropTypes.number,
         unstyled: PropTypes.bool,
-				onClick: PropTypes.function
+        onClick: PropTypes.function
     }
 
     static defaultProps = {
