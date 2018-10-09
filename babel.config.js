08fd7004a7b8842369a7bae73854b272acf53766
Jorgen Evens <jorgen@evens.eu>
2018-10-09 17:34:34 +0200

    [browsers] Allow babel to inject polyfills

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,8 @@ module.exports = {
     "presets": [
         ["@babel/preset-env", {
             "targets": "> 0.25%, not dead",
-            "modules": BABEL_MODULES
+            "modules": BABEL_MODULES,
+            "useBuiltIns": "usage"
         }],
         "@babel/preset-react"
     ],
